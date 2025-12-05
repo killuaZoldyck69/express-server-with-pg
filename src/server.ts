d5f1821fcc8e3c6e4ce81f78b6b3fcf70efb47c2
killuaZolyck69 <tanver694225@gmail.com>
2025-12-05 12:05:35 +0600

    add config and db file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
-import { Pool } from "pg";
-import dotenv from "dotenv";
-import path from "path";
+import config from "./config";
+import initDB from "./config/db";
 const app = express();
-const port = 5000;
-
-dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 // parser
 app.use(express.json());
@@ -16,38 +12,7 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
-// DB connection
-const pool = new Pool({
-  connectionString: `${process.env.CONNECTION_STR}`,
-});
-
-// create table
-const initDB = async () => {
-  await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `);
-  await pool.query(`
-        CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `);
-};
-
+// initialize DB
 initDB();
 
 app.get("/", logger, (req: Request, res: Response) => {
@@ -193,6 +158,6 @@ app.use((req: Request, res: Response) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(config.port, () => {
+  console.log(`Example app listening on port ${config.port}`);
 });
